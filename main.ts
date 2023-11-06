2bbde14138e8ecb118c189f2da39aab2c144d764
FabioKleis <fabiohkrc@gmail.com>
2023-11-06 13:58:35 -0300

    mandado protobuf via mqtt

--- main.ts
+++ main.ts
@@ -6,35 +6,38 @@ import { connect } from "mqtt";
 
 async function proto() {
     try {
-    //const fproto_json = await load("protofile.json");
-    //console.log(fproto_json);
+        const fproto_json = await load("protofile.json");
+        console.log(fproto_json);
 
-    const fproto = await load('protofile.proto');
-    console.log(fproto);
+        // const fproto = await load('protofile.proto');
+        // console.log(fproto);
 
-    var AwesomeMessage = fproto.lookupType("awesomepackage.AwesomeMessage");
+        var AwesomeMessage = fproto_json.lookupType("awesomepackage.AwesomeMessage");
 
-    var payload = { awesomeField: "AwesomeString" };
+        var payload = { awesomeField: "meu teste de protobuf via mqtt" };
     
-    
-    AwesomeMessage.verify(payload);
+        
+        var e = AwesomeMessage.verify(payload);
+        if (e)
+            throw Error(e);
+        
 
+        var msg = AwesomeMessage.create(payload);
 
-    var msg = AwesomeMessage.create(payload);
-    console.log(msg);
+        var buffer = AwesomeMessage.encode(msg).finish();
+        console.log(buffer);
 
-    var buffer = AwesomeMessage.encode(msg).finish();
-    console.log(buffer);
+        var msg = AwesomeMessage.decode(buffer);
+        console.log(msg);
 
-    var msg = AwesomeMessage.decode(buffer);
-    console.log(msg);
-    
+        mqtt(JSON.stringify(msg.toJSON()));
+        
     } catch (err) {
         throw err;
     }
 }
 
-function mqtt() {
+function mqtt(msg: string) {
 
     const client = connect("mqtt://broker.emqx.io");
 
@@ -42,7 +45,7 @@ function mqtt() {
         client.subscribe("testtopic/+"); // all levels
         
         
-        client.publish("testtopic/69", "hello!", { qos: 1 }, (err) => {
+        client.publish("testtopic/69", msg, { qos: 1 }, (err) => {
             if (err)
                 throw err;
         });
@@ -58,5 +61,4 @@ function mqtt() {
 
 
 proto();
-mqtt();
 
