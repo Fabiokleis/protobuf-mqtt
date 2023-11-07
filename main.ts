1fb3fbc118d41666938779cbb3ee3ef331d9095b
Fabio Kleis <fabiohkrc@gmail.com>
2023-11-07 13:30:39 -0300

    mandado buffer diretamente no mqtt

--- main.ts
+++ main.ts
@@ -3,18 +3,20 @@ import { load } from "protobufjs";
 import { connect } from "mqtt";
 
 
-
 async function proto() {
     try {
-        const fproto_json = await load("protofile.json");
-        console.log(fproto_json);
+        // const fproto_json = await load("protofile.json");
+        // console.log(fproto_json);
 
-        // const fproto = await load('protofile.proto');
-        // console.log(fproto);
+        const fproto = await load('protofile.proto');
+        console.log(fproto);
 
-        var AwesomeMessage = fproto_json.lookupType("awesomepackage.AwesomeMessage");
+        var AwesomeMessage = fproto.lookupType("awesomepackage.AwesomeMessage");
 
-        var payload = { awesomeField: "meu teste de protobuf via mqtt" };
+        var payload = { 
+            awesomeField: "meu teste de protobuf via mqtt",
+            deviceName: "123i102dkiaso0kd12"
+        };
     
         
         var e = AwesomeMessage.verify(payload);
@@ -24,20 +26,22 @@ async function proto() {
 
         var msg = AwesomeMessage.create(payload);
 
-        var buffer = AwesomeMessage.encode(msg).finish();
+        var buffer = Buffer.from(AwesomeMessage.encode(msg).finish());
+
         console.log(buffer);
+        
 
         var msg = AwesomeMessage.decode(buffer);
-        console.log(msg);
-
-        mqtt(JSON.stringify(msg.toJSON()));
+        // console.log(msg);
+        mqtt(buffer);
+        
         
     } catch (err) {
         throw err;
     }
 }
 
-function mqtt(msg: string) {
+function mqtt(msg: Buffer) {
 
     const client = connect("mqtt://broker.emqx.io");
 
@@ -45,7 +49,7 @@ function mqtt(msg: string) {
         client.subscribe("testtopic/+"); // all levels
         
         
-        client.publish("testtopic/69", msg, { qos: 1 }, (err) => {
+        client.publish("testtopic/69692", msg, { qos: 1 }, (err) => {
             if (err)
                 throw err;
         });
